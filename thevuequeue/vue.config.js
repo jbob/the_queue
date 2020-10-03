18f0f541b4b252a242058d997f9f0e1e7f5702c4
Roland Hummel <roland.hummel@qaware.de>
2020-10-03 21:44:32 +0200

    Add Vuetify

--- thevuequeue/vue.config.js
+++ thevuequeue/vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    outputDir: '../public/thevuequeue',
-    publicPath: '/thevuequeue/',
-    devServer: {
-        proxy: 'http://localhost:3000'
-    }
+  outputDir: "../public/thevuequeue",
+  publicPath: "/thevuequeue/",
+  devServer: {
+    proxy: "http://localhost:3000"
+  },
+  transpileDependencies: ["vuetify"]
 };
