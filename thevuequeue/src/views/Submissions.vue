e312ca1bc6f65812c5675f716f8d80d3c62887e3
Roland Hummel <roland.hummel@qaware.de>
2020-10-05 08:15:42 +0200

    First rough interaction with the submissions endpoint

--- thevuequeue/src/views/Submissions.vue
+++ thevuequeue/src/views/Submissions.vue
@@ -3,11 +3,44 @@
     <v-row>
       <v-col>
         <h1>Submissions</h1>
-        <p>
-          Not really implemented yet. Please go to the classic Perl-driven site:
-          <v-btn text href="/submissions">Submissions</v-btn>
-        </p>
+        <div v-if="!done">{{ status }}</div>
+      </v-col>
+    </v-row>
+    <v-row v-if="done">
+      <v-col v-for="(sub, index) in submissions" :key="index">
+        {{ sub.title }}
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: "",
+      submissions: []
+    };
+  },
+  computed: {
+    done() {
+      return this.status === "ready";
+    }
+  },
+  mounted() {
+    this.status = "loading";
+    fetch("/submissions", {
+      credentials: "same-origin",
+      headers: { accept: "application/json" }
+    })
+      .then(resp => resp.json())
+      .then(json => {
+        this.submissions = json.submissions;
+        this.status = "ready";
+      })
+      .catch(() => {
+        this.status = "failed";
+      });
+  }
+};
+</script>
