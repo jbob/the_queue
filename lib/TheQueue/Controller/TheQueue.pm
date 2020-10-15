9ad8a1ee3635aea3d87032caafc7b606b39b3fa1
Markus Koch <mail@markusko.ch>
2020-10-15 14:34:37 +0200

    Minor: Add error handling when a new/updated submissions link doesn't start with http:// or https://

--- lib/TheQueue/Controller/TheQueue.pm
+++ lib/TheQueue/Controller/TheQueue.pm
@@ -175,6 +175,8 @@ sub upsert {
     my $link     = $self->req->param('link') // '';
     my $comment  = $self->req->param('comment') // '';
 
+    return $self->reply->exception("The link must start with http:// or https://") if not $link =~ m|^https?://|;
+
     if ($id) {
 
         # Update existing record
